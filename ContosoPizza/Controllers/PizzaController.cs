93f17f418995a84518464b3c12bc64766985a82f
sardar awais <khanowais8888@outlook.com>
2022-02-01 15:43:54 +0500

    The preceding action:
    Responds only to the HTTP GET verb, as denoted by the [HttpGet] attribute.
    Queries the service for all pizza and automatically returns data with a Content-Type of application/json.

--- ContosoPizza/Controllers/PizzaController.cs
+++ ContosoPizza/Controllers/PizzaController.cs
@@ -9,9 +9,13 @@ namespace ContosoPizza.Controllers
     [ApiController]
     public class PizzaController : ControllerBase
     {
+        // we can get all the pizza service using this get method
         [HttpGet]
         public ActionResult<List<Pizza>> GetAll() =>
     PizzaService.GetAll();
 
+
+            
     }
+    // now another step is to get pizza service by id
 }
